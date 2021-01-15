1c42b753a5f3581541e112ac127a27dc3ba33a1f
Sonya Rumenova Radichkova <sonyaradichkova@gmail.com>
2021-01-15 10:24:44 -0500

    first commit

---
+++ Train_Timer/OffTimeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OffTimeViewController.swift
+//  Train_Timer
+//
+//  Created by Sonya Radichkova on 1/13/21.
+//
+
+import UIKit
+
+class OffTimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Train_Timer/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Train_Timer
+//
+//  Created by Sonya Radichkova on 1/7/21.
+//
+
+import UIKit
+var default_set_amount=5 //user should be able to change this - will fix later
+
+class ViewController: UIViewController {
+    var timer: Timer?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(fire), userInfo: nil, repeats: true)
+        Num_Sets_Text_Field.text="5"
+        
+    }
+    //invalidate timer when you're done with it
+    
+    @IBOutlet weak var Num_Sets_Text_Field: UITextField!
+    
+    
+    @IBAction func Decrease_Set_Amount(_ sender: UIButton) {
+        var num:Int?=Int(Num_Sets_Text_Field.text!) //shouldn't throw exception since we've set to 5 as default
+        if(num!>0){
+            num!-=1
+        }else{
+            num=0
+        }
+        Num_Sets_Text_Field.text=String(num!)
+
+    }
+    @IBAction func Increase_Set_Amount(_ sender: UIButton) {
+        var num:Int?=Int(Num_Sets_Text_Field.text!) //force unwrap
+        num!+=1 //increase
+        Num_Sets_Text_Field.text=String(num!)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer?.invalidate()
+    }
+    
+    @objc func fire()
+            {
+                print("FIRE!!!")
+            }
+
+}
+
