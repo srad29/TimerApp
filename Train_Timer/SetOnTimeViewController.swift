2d545d1bc90a9fa13a3d7c8995553c537e9d5fda
Sonya Rumenova Radichkova <sonyaradichkova@gmail.com>
2021-01-16 10:56:30 -0500

    added arrays for dial functions for off time

--- Train_Timer/SetOnTimeViewController.swift
+++ Train_Timer/SetOnTimeViewController.swift
@@ -7,15 +7,40 @@
 
 import UIKit
 
-class SetOnTimeViewController: UIViewController {
+class ArrayCreator{
+    static func formArrays(maxNumber: Int, unit: String) ->[String]{
+            var arr:[String]=[]
+            for num in 0...maxNumber{
+                let int_to_string=String(num)
+                arr[num]=int_to_string+" "+unit
+            }
+            return arr
+        }
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+class SetOnTimeViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
     
+    override func viewDidLoad() {
+            super.viewDidLoad()
 
+            // Do any additional setup after loading the view.
+        }
+    var selectedMins: String? //left dial for minutes
+    var minuteList=["0 min", "1 min", "2 min", "3 min", "4 min", "5 min"]
+    var selectedSeconds: String? //right dial for seconds
+//    var arrCreate=ArrayCreator()
+    var secondsList=ArrayCreator.formArrays(maxNumber: 59, unit: "sec")
+    //function currently takes in a string array; figure out how to make this more generic?
+    
+//    init() {
+//        secondsList=formArrays(maxNumber: 59, unit: "sec")
+//    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {}
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {}
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {}
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {}
     /*
     // MARK: - Navigation
 
